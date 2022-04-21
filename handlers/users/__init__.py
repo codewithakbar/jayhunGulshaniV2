392505f1daa7dda412f05cf38392885f83740def
AkbarAli <88106359+satipoff@users.noreply.github.com>
2022-04-21 12:37:05 +0500

    fixing bag

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -10,7 +10,7 @@ from .menu import dp
 from .catalog import dp
 from .delivery_status import dp
 from .sos import dp
-from .create_order import dp
+# from .create_order import dp
 
 
 __all__ = ['dp']
